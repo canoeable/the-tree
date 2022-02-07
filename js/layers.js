4914f4ab1a1952729ffbd2a3dc550cf889c0f4d1
theepichub <jameslangford464@gmail.com>
2022-02-07 12:20:00 +0000

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -405,7 +405,7 @@ addLayer("b", { // boosters
 })
 addLayer("a", { // QoL points
     name: "QoL points", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "TB", // This appears on the layer's node. Default is the id with the first letter capitalized
+    symbol: "QoL", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
